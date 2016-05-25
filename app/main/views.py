e5e5a4b29f6b8456ac8af6334a20f42283442a4a
wagyu <looker53@sina.com>
2016-05-25 15:19:11 +0800

    api test和selenium测试模拟浏览器环境

--- app/main/views.py
+++ app/main/views.py
@@ -8,6 +8,18 @@ from flask.ext.login import login_required,current_user
 from .. import db
 from ..decorators import admin_required,permission_required
 
+@main.route('/shutdown')
+def server_shutdown():
+    if not current_app.testing:
+        abort(404)
+    shutdown = request.environ.get('werkzeug.server.shutdown')
+    if not shutdown:
+        abort(500)
+    shutdown()
+    return 'Shutting down...'
+
+
+
 @main.route('/',methods=['GET','POST'])
 def index():
     form = PostForm()
