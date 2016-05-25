4591474a2b8e7cdc71e0063ffb5c7786604e0e56
wagyu <looker53@sina.com>
2016-05-25 23:40:58 +0800

    查询缓慢logger

--- app/main/views.py
+++ app/main/views.py
@@ -7,6 +7,18 @@ from .forms import EditProfileForm,EditProfileAdminForm,PostForm,CommentForm
 from flask.ext.login import login_required,current_user
 from .. import db
 from ..decorators import admin_required,permission_required
+from flask.ext.sqlalchemy import get_debug_queries
+
+@main.after_app.request
+def after_request(response):
+    for query in get_debug_queries():
+        if query.duration >= current_app.config['FLASKY_SLOW_DB_QUERY_TIME']:
+            current_app.logger.warning(
+                'Slow query:%s\nParameters: %s\nDuration: %fs\nContext: %s\n'
+                %(query.statement,query.parameters,query.duration,query.context)
+            )
+    return response
+
 
 @main.route('/shutdown')
 def server_shutdown():
